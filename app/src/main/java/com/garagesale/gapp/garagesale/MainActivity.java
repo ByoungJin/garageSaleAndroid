75c92400106f96f7be493ce253f350c74229a967
ByoungJin <kbj2043@gmail.com>
2017-06-15 22:58:42 +0900

    Add Menu 4 Refactoring

--- app/src/main/java/com/garagesale/gapp/garagesale/MainActivity.java
+++ app/src/main/java/com/garagesale/gapp/garagesale/MainActivity.java
@@ -1,35 +1,30 @@
 package com.garagesale.gapp.garagesale;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         // Floating Button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -41,30 +36,19 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        /*
-        // Drawer > 필요없을듯
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        // nav_view 와 Listener 연결
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        */
 
     }
 
-
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        // 판넬 열려있으면 닫음
+        SlidingUpPanelLayout slidingUpPanelLayout = (SlidingUpPanelLayout)findViewById(R.id.sliding_layout);
+        if(slidingUpPanelLayout.getPanelState().equals(SlidingUpPanelLayout.PanelState.EXPANDED)) {
+            slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
         } else {
             super.onBackPressed();
         }
+
     }
 
     @Override
@@ -72,117 +56,55 @@ public class MainActivity extends AppCompatActivity
 
         ActionBar actionBar = getSupportActionBar();
 
-        // Custom Actionbar를 사용하기 위해 CustomEnabled을 true 시키고 필요 없는 것은 false 시킨다
+        if (actionBar == null) return true;
+
+        // Custom Actionbar 사용 목적, CustomEnabled을 true 시키고 필요 없는 것 false
         actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
-        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
-        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
+        actionBar.setDisplayHomeAsUpEnabled(false);     //액션바 아이콘, 업 네비게이션 형태 표시
+        actionBar.setDisplayShowTitleEnabled(false);    //액션바에 표시되는 제목의 표시유무 설정
+        actionBar.setDisplayShowHomeEnabled(false);     //홈 아이콘 숨김처리.
 
-
-        //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
+        //Custom Layout을 Actionbar에 포팅
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        View actionbar = inflater.inflate(R.layout.custom_actionbar, null);
+        @SuppressLint("InflateParams") View actionbar = inflater.inflate(R.layout.custom_actionbar, null);
 
         actionBar.setCustomView(actionbar);
 
-        //액션바 양쪽 공백 없애기
+        //액션바 양쪽 공백 제거
         Toolbar parent = (Toolbar) actionbar.getParent();
         parent.setContentInsetsAbsolute(0, 0);
 
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection Simplifiablcontent_fragment_layouteIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        Fragment fragment = null;
-        String title = getString(R.string.app_name);
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-            fragment = new MainFragment();
-            title = "Main";
-        } else if (id == R.id.nav_gallery) {
-            fragment = new ProfileFragment();
-            title = "Profile";
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        if (fragment != null) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.content_fragment_layout, fragment);
-            ft.commit();
-        }
-
-        // set the toolbar title
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(title);
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
     public void btnStart(View v) {
         // 판넬 닫기
-        SlidingUpPanelLayout slidingUpPanelLayout = (SlidingUpPanelLayout)findViewById(R.id.sliding_layout);
-        String panelState = slidingUpPanelLayout.getPanelState().toString();    // COLLAPSED, EXPANDED
-        if(panelState.equals("EXPANDED")) {
-            View panel_layout = (View) findViewById(R.id.panel_drawer_layout);
-            panel_layout.performClick();
-        }
+        slideMenu(null);
 
         // Fragment Change
-
         int id = v.getId();
 
         Fragment fragment = null;
         String title = getString(R.string.app_name);
 
         if (id == R.id.main_button) {
-            // Handle the camera action
             fragment = new MainFragment();
             title = "Main";
         } else if (id == R.id.profile_button) {
             fragment = new ProfileFragment();
             title = "Profile";
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.join_button) {
+            fragment = new JoinFragment();
+            title = "Join";
+        } else if (id == R.id.login_button) {
+            fragment = new LoginFragment();
+            title = "Login";
+        } else if (id == R.id.setting_button) {
+            fragment = new SettingFragment();
+            title = "Setting";
+        } else if (id == R.id.store_button) {
+            fragment = new StoreFragment();
+            title = "Store";
         }
 
         if (fragment != null) {
@@ -191,15 +113,20 @@ public class MainActivity extends AppCompatActivity
             ft.commit();
         }
 
-        // set the toolbar title
+        // Set the toolbar title
         TextView titleTextView = (TextView) findViewById(R.id.title);
         titleTextView.setText(title);
 
     }
 
-    public void ExpandMenu(View v) {
-        View panel_layout = (View) findViewById(R.id.panel_drawer_layout);
-        panel_layout.performClick();
+    public void slideMenu(View v) {
+        // 판넬 열려있음 닫고 닫혀있음 열기
+        SlidingUpPanelLayout slidingUpPanelLayout = (SlidingUpPanelLayout)findViewById(R.id.sliding_layout);
+        if(slidingUpPanelLayout.getPanelState().equals(SlidingUpPanelLayout.PanelState.EXPANDED)) {
+            slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+        }else {
+            slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+        }
     }
 
 }
--- app/src/main/java/com/garagesale/gapp/garagesale/StoreFragment.java
+++ app/src/main/java/com/garagesale/gapp/garagesale/StoreFragment.java
@@ -6,12 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MainFragment extends Fragment {
+public class StoreFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_store, container, false);
 
     }
 }
